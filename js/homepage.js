d78c6b5a6d21eb7b2bbded7780f5aef98d027913
BwayCer <bwaycer@gmail.com>
2017-07-12 08:10:50 +0800

    v0.0.0-2e255b7

--- js/homepage.js
+++ js/homepage.js
@@ -8,44 +8,97 @@
 
 
 !function() {
+    var isPcMode = !~navigator.userAgent.indexOf( 'Mobile' );
+
     !function() {
-        var parentClassList = document.getElementById( 'readDocList' ).classList;
+        var bodyClassList = document.body.classList;
         var drawId = null;
 
-        function modeSwitch( mode ) {
-            var fnDraw;
-
-            if ( drawId ) cancelAnimationFrame( drawId );
-
-            switch ( mode ) {
-                case 'PC':
-                    fnDraw = function () {
-                        drawId = null;
-                        parentClassList.add( 'onPCDoc' );
-                        parentClassList.remove( 'onMobileDoc' );
-                    };
-                    break;
-                case 'Mobile':
-                    fnDraw = function () {
-                        drawId = null;
-                        parentClassList.add( 'onMobileDoc' );
-                        parentClassList.remove( 'onPCDoc' );
-                    };
-                    break;
-            }
-
-            requestAnimationFrame( fnDraw );
-        }
+        if ( isPcMode ) bodyClassList.add( 'onPCDoc' );
+        else bodyClassList.add( 'onMobileDoc' );
 
         document.getElementById( 'readDocList_pcDocButton' )
             .addEventListener( 'click', function () {
-                modeSwitch( 'PC' );
+                if ( drawId ) cancelAnimationFrame( drawId );
+
+                requestAnimationFrame( function () {
+                    drawId = null;
+                    bodyClassList.add( 'onPCDoc' );
+                    bodyClassList.remove( 'onMobileDoc' );
+                } );
             } );
 
         document.getElementById( 'readDocList_mobileDocButton' )
             .addEventListener( 'click', function () {
-                modeSwitch( 'Mobile' );
+                if ( drawId ) cancelAnimationFrame( drawId );
+
+                requestAnimationFrame( function () {
+                    drawId = null;
+                    bodyClassList.add( 'onMobileDoc' );
+                    bodyClassList.remove( 'onPCDoc' );
+                } );
             } );
+    }();
+
+    // 緩載入圖片
+    !function() {
+        var substituteImg = URL.createObjectURL( new Blob( [
+            '<svg width="96" height="96" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">'
+            +  '<circle class="loader_spinner" cx="32" cy="32" r="14"'
+            +    ' fill="none" stroke="#FFD306" stroke-width="3px" stroke-dashoffset="48">'
+            +    '<animate attributeName="stroke-dasharray"'
+            +      ' values="2,85.964;65.973,21.9911;2,85.964" dur="1.6s" repeatCount="indefinite" />'
+            +    '<animateTransform attributeName="transform" type="rotate"'
+            +      ' values="0,32,32;180,32,32;720,32,32" dur="1.6s" repeatCount="indefinite" />'
+            +  '</circle>'
+            + '</svg>'
+        ], { type: 'image/svg+xml' } ) );
+
+        function lazyloadImg( helItem ) {
+            var p, len, helClone, itemSrc;
+
+            itemSrc = helItem.dataset.lazyloadSrc;
+
+            if ( itemSrc ) {
+                if ( !helItem.src ) helItem.src = substituteImg;
+
+                helClone = helItem.cloneNode();
+                delete helClone.dataset.lazyloadSrc;
+                helClone.src = itemSrc;
+
+                function putBack() {
+                    helClone.onload = null;
+                    helItem.parentNode.insertBefore( helClone, helItem );
+                    helItem.remove();
+                }
+
+                if ( helClone.complete ) putBack();
+                else helClone.onload = putBack;
+            }
+        }
+
+        var helInstallDoc_pc = document.getElementById( 'installDoc_pc' );
+        var helInstallDoc_mobile = document.getElementById( 'installDoc_mobile' );
+
+        if ( isPcMode ) {
+            helInstallDoc_pc.querySelectorAll( 'img' ).forEach( lazyloadImg );
+
+            document.getElementById( 'readDocList_mobileDocButton' )
+                .addEventListener( 'click', function tem() {
+                    this.removeEventListener( 'click', tem );
+
+                    helInstallDoc_mobile.querySelectorAll( 'img' ).forEach( lazyloadImg );
+                } );
+        } else {
+            helInstallDoc_mobile.querySelectorAll( 'img' ).forEach( lazyloadImg );
+
+            document.getElementById( 'readDocList_pcDocButton' )
+                .addEventListener( 'click', function tem() {
+                    this.removeEventListener( 'click', tem );
+
+                    helInstallDoc_pc.querySelectorAll( 'img' ).forEach( lazyloadImg );
+                } );
+        }
     }();
 }();
 
